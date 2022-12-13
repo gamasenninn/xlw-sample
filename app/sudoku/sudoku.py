865e4af9d58fed703cebb456376ea47631cb0aef
gamasenninn <gamasenninn@yahoo.co.jp>
2022-12-13 12:04:56 +0900

    数独最終形

--- app/sudoku/sudoku.py
+++ app/sudoku/sudoku.py
@@ -1,55 +1,40 @@
 import xlwings as xw
-import pandas as pd
 
-input_grid = []
-
-def find_next_idx(grid):
+def next_idx(board):
     for y in range(9):
         for x in range(9):
-            if grid[y][x] == 0:
+            if board[y][x] == 0:
                 return y, x
     return -1, -1
 
-def is_valid(grid, y, x, value):
-    if value not in grid[y]:
-        if value not in [i[x] for i in grid]:            
-            blk_x, blk_y = (x//3) * 3, (y//3) * 3
-            blk_grid = [i[blk_x:blk_x + 3] for i in grid[blk_y:blk_y + 3]]
-            if value not in sum(blk_grid, []):
-                return True
-    return False
-
-def solve_sudoku(grid, y=0, x=0):
-    y, x = find_next_idx(grid)
-    if y == -1 or x == -1:
+def check_val(board, y, x, v):
+    if v not in board[y] and v not in [row[x] for row in board]:
+        sx, sy = (x//3) * 3, (y//3) * 3
+        for bky in board[sy:sy+3]:
+            if v in bky[sx:sx+3]:
+                return False
         return True
-    for value in range(1, 10):
-        if is_valid(grid, y, x, value):
-            grid[y][x] = value
-            #xw.Range((y+1, x+16)).value = value
-            if solve_sudoku(grid, y, x):
-                return True
-            grid[y][x] = 0
-            #xw.Range((y+1, x+16)).value = ''
     return False
 
-def copy_grid(grid):
-    global input_grid
-    input_grid = [[0 if v is None else v for v in row] for row in xw.Range('A1:I9').value]
-    xw.Range('P1').value = [['' if v is None else v for v in row] for row in xw.Range('A1:I9').value]
+def solve_sd(board, y, x):
+    y, x = next_idx(board)
+    if -1 in [y,x]: 
+        return True     
+    for val in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        if check_val(board, y, x, val):
+            board[y][x] = val
+            if solve_sd(board, y, x):
+                return True
+            board[y][x] = 0
+    return False
 
 def main():
-    global wb
     wb = xw.Book.caller()
     wb.sheets['数独'].activate()
-
-    copy_grid(input_grid)
-    solve_sudoku(input_grid)
-    xw.Range('P1').value = input_grid
-
+    i_board = [[0 if v is None else int(v) for v in row] for row in xw.Range('A1:I9').value]
+    solve_sd(i_board,0,0)
+    xw.Range('P1').value = i_board
 
 if __name__ == "__main__":
     xw.Book("sudoku.xlsx").set_mock_caller()
     main()
-
-
